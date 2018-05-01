ad77a03e705fc747fe4b54ed92410ea7a8e78222
Robert Lowe <robertmlowe@rmlowe.com>
2018-05-01 10:18:37 +0800

    Annotations when calling Kotlin from Java

--- src/academy/learnprogramming/callkotlinfromjava/Main.java
+++ src/academy/learnprogramming/callkotlinfromjava/Main.java
@@ -1,7 +1,10 @@
 package academy.learnprogramming.callkotlinfromjava;
 
 import academy.learnprogramming.kotlincode.Car;
+import academy.learnprogramming.kotlincode.SingletonObj;
 import academy.learnprogramming.kotlincode.StaticCar;
+
+import java.io.IOException;
 
 public class Main {
 
@@ -11,8 +14,25 @@ public class Main {
         StaticCar.print("print this Java string");
 
         Car car = new Car("blue", "BMW", 2011);
-        System.out.println(car.getModel());
+        System.out.println(car.model);
         System.out.println(car.getColor());
+
+        Car.carComp();
+        System.out.println("isAuto = " + Car.constant);
+
+        SingletonObj.doSomething();
+
+        //car.printMe(null);
+
+        try {
+            StaticCar.doIO();
+        }
+        catch (IOException e) {
+            System.out.println("IOException!");
+        }
+
+        StaticCar.defaultArgs("The Number is: ");
+
 
     }
 }
--- src/academy/learnprogramming/kotlincode/Car.kt
+++ src/academy/learnprogramming/kotlincode/Car.kt
@@ -2,13 +2,41 @@
 
 package academy.learnprogramming.kotlincode
 
+import java.io.IOException
+
 fun topLevel() = println("I'm in the Car file!")
 
 fun main(args: Array<String>) {
     "Print this".print()
 }
 
-class Car(val color: String, val model: String, val year: Int) {
+@JvmOverloads fun defaultArgs(str: String, num: Int = 25) {
+
+}
+
+@Throws(IOException::class)
+fun doIO() {
+    throw IOException()
+}
+
+object SingletonObj {
+    @JvmStatic fun doSomething() = println("I'm doing something in the singleton object")
+}
+
+class Car(val color: String, @JvmField val model: String, val year: Int) {
+
+    companion object {
+        const val constant = 25
+        @JvmField val isAuto = false
+        @JvmStatic fun carComp() = println("I'm in Car's companion object")
+    }
+
+    fun printMe(text: String) {
+        println("I don't expect a null value")
+    }
+
+
+
 //    var color: String = color
 //    private set(value) {
 //        field = "always green"
